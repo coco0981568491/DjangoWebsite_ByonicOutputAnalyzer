4bd7794be562d3ce7719a660ea3d0a584631d546
yuxitsai <coco0981568491@gmail.com>
2021-05-01 03:44:44 +0800

    tweaked app for heroku

--- mysite/worker.py
+++ mysite/worker.py
@@ -4,7 +4,7 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDISTOGO_URL', 'redis://redistogo:8e24889b2c9758e0d3d4a5a6f216005a@dory.redistogo.com:9414')
+redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 
 conn = redis.from_url(redis_url)
 
