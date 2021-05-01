a04acf58c63554fbd2035d67f990f8821d0f607c
yuxitsai <coco0981568491@gmail.com>
2021-05-01 15:41:19 +0800

    tweaked app for heroku

--- website/settings.py
+++ website/settings.py
@@ -135,6 +135,17 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
 
+redis_url = urlparse.urlparse(os.environ.get('REDISTOGO_URL', 'redis://localhost:6959'))
 
+CACHES = {
+    'default': {
+        'BACKEND': 'redis_cache.RedisCache',
+        'LOCATION': '%s:%s' % (redis_url.hostname, redis_url.port),
+        'OPTIONS': {
+            'DB': 0,
+            'PASSWORD': redis_url.password,
+        }
+    }
+}
 
 
