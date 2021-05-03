f8a7bf1ee8a7541186184d86e67b344bbcde270d
yuxitsai <coco0981568491@gmail.com>
2021-05-03 21:24:26 +0800

    tweaked app for heroku

--- website/settings.py
+++ website/settings.py
@@ -143,4 +143,4 @@ CELERY_TASK_SERIALIZER = 'pickle'
 
 # use heroku redis as the broker
 app.conf.update(BROKER_URL='redis://:p8d4eb3a43ddd9a00cf4024e7ee94b80f52d92f851aa85788f881daaeda71dd7b@ec2-54-80-245-74.compute-1.amazonaws.com:6929',
-                CELERY_RESULT_BACKEND='django-db')
+                CELERY_RESULT_BACKEND='redis://:p8d4eb3a43ddd9a00cf4024e7ee94b80f52d92f851aa85788f881daaeda71dd7b@ec2-54-80-245-74.compute-1.amazonaws.com:6929')
