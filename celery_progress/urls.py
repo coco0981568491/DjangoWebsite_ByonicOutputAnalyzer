8f138d5c8a0d4578abf2c05b1f04be6853a092d3
yuxitsai <coco0981568491@gmail.com>
2021-05-03 23:41:41 +0800

    tweaked app for heroku

--- celery_progress/urls.py
+++ celery_progress/urls.py
@@ -1,7 +1,7 @@
-from django.urls import re_path
+from django.urls import path
 from . import views
 
 app_name = 'celery_progress'
 urlpatterns = [
-    re_path(r'^(?P<task_id>[\w-]+)/$', views.get_progress, name='task_status'),
+    path('', views.get_progress, name='task_status'),
 ]
