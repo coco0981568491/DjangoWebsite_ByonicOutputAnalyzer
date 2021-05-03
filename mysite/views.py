90090a9624743f9c0d2e30d5ddddb33a615f6ccd
yuxitsai <coco0981568491@gmail.com>
2021-05-04 01:24:27 +0800

    tweaked app for heroku

--- mysite/views.py
+++ mysite/views.py
@@ -13,41 +13,43 @@ def home(request):
 	if request.method == "POST":
 
 		# get data and name it as file for convenience 
-		# file = request.FILES["myFile"]
-		# filename = file.name
+		file = request.FILES["myFile"]
+		filename = file.name
 
 		# Celery does not know how to serialize complex objects such as file objects. 
 		# However, this can be solved pretty easily. 
 		# What I do is to encode/decode the file to its Base64 string representation. 
 		# This allows me to send the file directly through Celery.
 
-		# file_bytes = file.read()
-		# file_bytes_base64 = base64.b64encode(file_bytes)
-		# file_bytes_base64_str = file_bytes_base64.decode('utf-8') # this is a str
+		file_bytes = file.read()
+		file_bytes_base64 = base64.b64encode(file_bytes)
+		file_bytes_base64_str = file_bytes_base64.decode('utf-8') # this is a str
 
 		
 
 		# (...send string through Celery...)
-		# task = data_processing.delay(file_bytes_base64_str, filename)
+		task = data_processing.delay(file_bytes_base64_str, filename)
 
 		# data_processing.delay(file_bytes_base64_str, filename)
 
-		# res = AsyncResult(task)
+		res = AsyncResult(task)
 
-		task = test.delay(100)
+		# task = test.delay(100)
 
-		return render(request, "progress.html", context={'task_id': task.task_id})
+		# return render(request, "progress.html", context={'task_id': task.task_id})
 
 		# check if the task has been finished
-		# if res.ready(): 
+		if res.ready(): 
 
-		# 	resp = HttpResponse(res.get(), content_type = 'application/x-zip-compressed')
-		# 	resp['Content-Disposition'] = 'attachment; filename=%s'%zip_filename
+			zip_filename = 'Results.zip'
 
-		# 	return resp
+			resp = HttpResponse(res.get(), content_type = 'application/x-zip-compressed')
+			resp['Content-Disposition'] = 'attachment; filename=%s'%zip_filename
 
-		# else:
-		# 	return render(request, "progress.html")
+			return resp
+
+		else:
+			return render(request, "progress.html", context={'task_id': task.task_id})
 
 	else:
 		return render(request, "index.html")
