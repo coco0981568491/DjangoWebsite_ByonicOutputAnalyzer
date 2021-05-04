6a07580a7a0701b7b38899fad40b5ac58be96428
yuxitsai <coco0981568491@gmail.com>
2021-05-04 14:47:43 +0800

    tweaked app for heroku

--- mysite/views.py
+++ mysite/views.py
@@ -40,17 +40,18 @@ def home(request):
 		# check if the task has been finished
 		if task.ready(): 
 
-			zip_filename = 'Results.zip'
+			# zip_filename = 'Results.zip'
 
-			# convert results in base64 str back into bytes
-			results_bytes_base64 = task.get().encode('utf-8')
-			results_bytes = base64.b64decode(results_bytes_base64)
+			# # convert results in base64 str back into bytes
+			# results_bytes_base64 = task.get().encode('utf-8')
+			# results_bytes = base64.b64decode(results_bytes_base64)
 			
 
-			resp = HttpResponse(results_bytes, content_type = 'application/x-zip-compressed')
-			resp['Content-Disposition'] = 'attachment; filename=%s'%zip_filename
+			# resp = HttpResponse(results_bytes, content_type = 'application/x-zip-compressed')
+			# resp['Content-Disposition'] = 'attachment; filename=%s'%zip_filename
 
-			return resp
+			# return resp
+			return render(request, "index.html")
 
 		else:
 			return render(request, "progress.html", context={'task_id': task.task_id})
