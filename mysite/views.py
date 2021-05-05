6cf3a835f8641e6d69ac03966f7a19e3e7f25eb1
yuxitsai <coco0981568491@gmail.com>
2021-05-06 01:54:39 +0800

    tweaked app for heroku

--- mysite/views.py
+++ mysite/views.py
@@ -59,17 +59,17 @@ def download(request, results):
 
 	# results = task.get()
 
-	# zip_filename = 'Results.zip'
+	zip_filename = 'Results.zip'
 
-	# # convert results in base64 str back into bytes
-	# results_bytes_base64 = results.encode('utf-8')
-	# results_bytes = base64.b64decode(results_bytes_base64)
+	# convert results in base64 str back into bytes
+	results_bytes_base64 = results.encode('utf-8')
+	results_bytes = base64.b64decode(results_bytes_base64)
 	
 
-	# resp = HttpResponse(results_bytes, content_type = 'application/x-zip-compressed')
-	# resp['Content-Disposition'] = 'attachment; filename=%s'%zip_filename
+	resp = HttpResponse(results_bytes, content_type = 'application/x-zip-compressed')
+	resp['Content-Disposition'] = 'attachment; filename=%s'%zip_filename
 
-	# return resp
-	return render(request, "test.html")
+	return resp
+	# return render(request, "test.html")
 	# resp = HttpResponse("status is SUCCESS!")
 	# return resp
