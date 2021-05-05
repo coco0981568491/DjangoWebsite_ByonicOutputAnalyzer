a09459257f07376fd1d7474618b3502eccbcb5c4
yuxitsai <coco0981568491@gmail.com>
2021-05-05 18:13:24 +0800

    tweaked app for heroku

--- website/settings.py
+++ website/settings.py
@@ -165,7 +165,8 @@ app.conf.update(BROKER_URL='redis://:p8d4eb3a43ddd9a00cf4024e7ee94b80f52d92f851a
 
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'my_cache_table',
     }
 }
 
